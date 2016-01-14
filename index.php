6bad3ba704acb6363f1ff1c624250f5b638fecf1
Tom Wicks <tom@miln.co>
2016-01-14 17:07:14 +0000

    HTML written

--- index.php
+++ index.php
@@ -40,6 +40,198 @@
 
 </header>
 
-<article class=""
+<section class="home">
+
+	<div class="grid-container">
+
+		<div class="col-8">
+
+			<h1>Building More</h1>
+
+			<p class="large">Helping bring our clients vision to life. Undertaking projects of all sizes throughout Baqueira and Mallorca.</p>
+
+			<a class="link-btn-one">Find Out More</a>
+
+		</div> <!-- col-8 -->
+
+	</div> <!-- grid-container -->
+
+	<img src=""/>
+
+</section> <!-- home-splash -->
+
+<section class="about">
+
+	<div class="grid-container">
+
+		<div class="col-8">
+
+			<h2>About</h2>
+
+			<p class="large">With over 40 years combined experience in the building industry we can help you with your project. From residential properties, to refurbishment and new builds, our aim is to help you build more.
+</br>Working in the areas of Baqueira and Mallorca, contact us today to talk about your project </p>
+
+		</div> <!-- col-10 -->
+
+	</div> <!-- grid-container -->
+
+	<img src=""/>
+
+</section> <!-- about -->
+
+<section class="what">
+
+	<div class="grid-container">
+
+		<div class="row">
+
+			<div class="col-8">
+
+				<h2>What We Do</h2>
+
+				<p class="large">From Residential and new builds to complete refurbishments, and everything inbetween – we can make your ideas come to life.</p>
+
+			</div> <!-- col-8 -->
+
+		</div> <!-- row -->
+
+		<div class="row">
+
+			<div class="col-8">
+
+				<h3>Residential</h3>
+
+				<p>With over 40 years combined experience in the building industry we can help you with your project. From residential properties, to  refurbishment and new builds, our aim is to help you build more for tomorrow.</p>
+
+			</div>
+
+			<div class="col-4">
+
+				<img src=""/>
+
+			</div>
+
+		</div> <!-- row -->
+
+		<div class="row">
+
+			<div class="col-8">
+
+				<h3>New Build &amp; Total Conversion</h3>
+
+				<p>With over 40 years combined experience in the building industry we can help you with your project. From residential properties, to  refurbishment and new builds, our aim is to help you build more for tomorrow.</p>
+
+			</div>
+
+			<div class="col-4">
+
+				<img src=""/>
+
+			</div>
+
+		</div> <!-- row -->
+
+		<div class="row">
+
+			<div class="col-8">
+
+				<h3>Refurbishment</h3>
+
+				<p>With over 40 years combined experience in the building industry we can help you with your project. From residential properties, to  refurbishment and new builds, our aim is to help you build more for tomorrow.</p>
+
+			</div>
+
+			<div class="col-4">
+
+				<img src=""/>
+
+			</div>
+
+		</div> <!-- row -->
+
+		<div class="row">
+
+			<div class="col-8">
+
+				<h3>Management &amp; Maintenance</h3>
+
+				<p>With over 40 years combined experience in the building industry we can help you with your project. From residential properties, to  refurbishment and new builds, our aim is to help you build more for tomorrow.</p>
+
+			</div>
+
+			<div class="col-4">
+
+				<img src=""/>
+
+			</div>
+
+		</div> <!-- row -->
+
+	</div> <!-- grid-container -->
+
+	<img src=""/>
+
+</section> <!-- what -->
+
+<section class="contact">
+
+	<div class="grid-container">
+
+		<div class="row">
+
+			<div class="col-8">
+
+				<h2>Contact</h2>
+
+				<p class="large">If you are interested in working on a project together, please get in contact on the details below.</p>
+
+			</div> <!-- col-8 -->
+
+		</div> <!-- row -->
+
+		<div class="row">
+
+			<div class="col-8">
+
+				<form>
+
+					<fieldset class="form-half">
+						<label>Name</label>
+						<input type="text"></input>
+					</fieldset>
+
+					<fieldset class="form-half">
+						<label>Company</label>
+						<input type="text"></input>
+					</fieldset>
+
+					<fieldset class="form-half">
+						<label>Email</label>
+						<input type="text"></input>
+					</fieldset>
+
+					<fieldset class="form-half">
+						<label>Subject</label>
+						<input type="text"></input>
+					</fieldset>
+
+
+					<fieldset class="form-full">
+						<label>Message</label>
+						<textarea></textarea>
+					</fieldset>
+
+
+					<input type="submit" value="Submit">
+
+				</form>
+
+			</div> <!-- col-8 -->
+
+		</div> <!-- row -->
+
+	</div> <!-- grid-container -->
+
+</section> <!-- Contact -->
 
 <?php include 'includes/global/footer.php'; ?>
